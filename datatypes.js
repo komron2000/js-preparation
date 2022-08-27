484b416593ebe5359a9ba8103427eb98c958cd0c
Kamron200 <96528746+komron2000@users.noreply.github.com>
2022-08-27 11:39:42 +0300

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,80 @@
+//День 2: Упражнения
+///Упражнение: Уровень 1
+// console.log("30 дней JavaScript"); //2
+// let string = "30 дней JavaScript";
+// console.log(string.length);//3
+// console.log("Hello");
+
+// let string = " 30 дней JavaScript ";  
+// let nn = "30 дней JavaScript";
+// console.log(string.toUpperCase());//4
+// console.log(string.toLowerCase());//5
+// console.log(string.substr(0,7)); //6
+// console.log(string.substring(3,6)); //6
+// let companiy = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+// let string = "30 Days of JavaScript"; //7
+// console.log(string.includes("Script")); //8
+// console.log(string.split()); //9
+// console.log(string.split("")); //10
+// console.log(companiy.split("")); //11
+// console.log(string.replace("JavaScript","Python")); //12
+// console.log(string.charCodeAt("j")); //13
+// console.log(string.charAt(15));//14
+// console.log(string.indexOf()); //15
+// console.log(string.lastIndexOf());//16
+
+// let string = "Вы не можете закончить предложение, потому что, потому что, потому что это соединение";
+// console.log(string.indexOf("потому что")); // 17
+// console.log(string.lastIndexOf("потому что")); //18
+// console.log(string.search("потому что")); //19
+// console.log(string.trim()); //20
+// console.log(string.repeat());//25
+
+// let string = "Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.";
+// console.log(string);//1
+// console.log(string);//2
+// console.log(typeof(10));//3
+// console.log(Math.round(9.8));//4
+// let string = "Вы не можете закончить предложение, потому что потому что потому что это соединение";
+// console.log(string.includes("on"));//5
+// console.log(string.includes("jargon"));//6
+// console.log(Math.random(0,100));//7
+// console.log(Math.random(50,100));//8
+// console.log(Math.random(0,255));//9
+// console.log(Math.random("JavaScript",100));//10
+// console.log(string.substr("потому что потому что"));//12
+// let string = "Вы не можете закончить предложение, потому что, потому что, потому что это соединение";
+// console.log(string.search("любовь")); //1
+// console.log(string.match());//2
+
+
+// const sentence =
+//   "%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching";
+//   console.log(sentence);
+//   console.log("Он зарабатывает 5000 евро с зарплаты в месяц, годовой бонус в 10000 евро, онлайн-курсы в 15000 евро в месяц");//4
+
+
+//Упражнения: уровень 2
+// Задание 2
+// let a =5, b = 4, c = 3;
+// S = a+b+c;
+// console.log(S); //12
+
+
+// Задание 9
+// let hour;
+// const agree = prompt("вывидете часы: ");
+// const agree2 = prompt("вывидете ставку в час: ");
+//  hour = (agree*agree2);
+// console.log(hour);
+
+
+//Задание 10
+let name;
+const LengthName = prompt("выдите пожалуйста ваше имя: ");
+let ss = LengthName+name;
+if(7 == name){
+    console.log("ваше имя чотки");
+}else{
+    console.log("ваше имя длиный");
+}
---
+++ variable.js
@@ -0,0 +1,271 @@
+// let fristName;
+// console.log(fristName);
+// console.log(typeof"Asabeneh")
+// console.log(true);
+
+// let firstName = "Asabeneh"; // имя человека
+// let lastName = "Yetayeh"; // фамилия человека
+// let country = "Finland"; // страна
+// let city = "Helsinki"; // столица
+// let ag2e = 100; // возраст в годах
+// let isMarried = true;
+
+// console.log(firstName, lastName, country, city, ag2e, isMarried);
+
+// let age = 100; // возраст в годах
+// const gravity = 9.81; // сила тяжести на землю в м/с2
+// const boilingPoint = 100; // температура кипения воды, температура в oC
+// const PI = 3.14; // геометрическая постоянная
+
+// console.log(gravity, boilingPoint, PI);
+
+
+//     name= 'Asabene',
+//     job='taecher',
+//     live= 'finland';
+//     console.log(name,job,live);
+  
+// let numOne = 3;
+// let numTwo = 3;
+
+// console.log(numOne == numTwo); // true
+
+// let js = "JavaScript";
+// let py = "Pyton";
+
+// console.log(js == py);
+
+// let lightOn = true;
+// let lightOff = false;
+
+// console.log(lightOn == lightOff);
+
+// let nums = [1,2,3];
+// let numbers = [1,2,3];
+
+// console.log(nums == numbers);
+// let userOne = {
+//     name:"Asabeneh",
+//     role:"teaching",
+//     country:"Finland"
+// };
+
+// let userTwo = {
+//     name:"Asabeneh",
+//     role:"teaching",
+//     country:"Finland"
+// };
+
+// console.log(userOne == userTwo);
+
+// let age = 35;
+// const gravity = 9.81;
+// let mass = 72;
+// const PI = 3.14;
+
+// // Больше примеров 
+// const boilingPoin = 100; // температура в oC, температура кипения воды которой постоянна
+// const bodyTemp = 37; // oC средняя температура тела человека, которая является постоянной
+
+// console.log(age, gravity,mass,PI, boilingPoin,bodyTemp);
+
+
+
+
+//  const PI = Math.PI;
+//  console.log(PI);
+
+
+// console.log(Math.round(PI)); //3,чтобы округлить до ближайшего числа
+// console.log(Math.round(9.81)); //10
+// console.log(Math.floor(PI)); // 3 округленние вниз
+// console.log(Math.ceil(PI)); // 4 округление вверх
+// console.log(Math.min(-5,3,20,4,5,10)); //-5, возрващает минамальное значение 
+// console.log(Math.max(-5,3,20,4,5,10)); //20, возращает максимальное значение
+
+
+// const randNum = Math.random(); // создает случайное число от 0 до 0,99999
+// console.log(randNum);
+
+// // Давайте создадим случайное число от 0 до 10
+
+// const num = Math.floor(Math.random() * 11); // создает случайное число от 0 до 10
+// console.log(num);
+
+// // Абсолютное значение
+// console.log(Math.abs(-10)); //10
+
+// // Квадратный корень
+// console.log(Math.sqrt(100)); // 10
+
+// console.log(Math.sqrt(2)); //1.4142135623730951
+
+// // Мощность
+// console.log(Math.pow(3, 2)); // 9
+
+// console.log(Math.E); // 2.718
+
+// // Логарифм
+// // Возвращает натуральный логарифм основания E из x, Math.log(x)
+// console.log(Math.log(2)); // 0.6931471805599453
+// console.log(Math.log(10)); // 2.302585092994046
+
+// // Тригонометрия
+// Math.sin(0);
+// Math.sin(60);
+
+// Math.cos(0);
+// Math.cos(60);
+
+// let randomNum = Math.random(); // генерирует от 0 до 0,999
+// let numBtnZeroAndTen = randomNum * 11;
+
+// console.log(numBtnZeroAndTen); // это дает: мин 0 и макс 10.99
+
+// let randomNumRoundToFloor = Math.floor(numBtnZeroAndTen);
+// console.log(randomNumRoundToFloor); // это дает от 0 до 10
+
+// let space = " ";
+// let firstName = "Asabeneh";
+// let lastName = "Yetayeh";
+// let country = "Finland";
+// let city = "Helsinki";
+// let language = "JavaScript";
+// let job = "teacher";
+
+// let fullName = firstName + space + lastName; // конкатенацияб объелинение двух строк.
+// console.log(fullName)
+
+// console.log(
+//     "I hope every one is enjoying the 30 Days Of JavaScript challenge.\nDo you ?"
+// ); // line break
+// console.log("Days\tTopics\tExercises");
+// console.log("Day 1\t3\t5");
+// console.log("Day 2\t3\t5");
+// console.log("Day 3\t3\t5");
+// console.log("Day 4\t3\t5");
+// console.log("This is a back slash  symbol (\\)"); // To write a back slash
+// console.log('In every programming language it starts with "Hello, World!"');
+// console.log("In every programming language it starts with 'Hello, World!'");
+// console.log("The saying 'Seeing is Believing' is't correct in 2020");
+
+
+// console.log(`The sum of 2 and 3 is 5`); // статическая запись данных
+// let a = 2;
+// let b = 3;
+// console.log(`The sum of ${a} and ${b} is ${a + b}`); // ввод данных динамически
+
+
+// let firstName = "Asabeneh";
+// let lastName = "Yetayeh";
+// let country = "Finland";
+// let city = "Helsinki";
+// let language = "JavaScript";
+// let job = "teacher";
+// let age = 250;
+// let fullName = firstName + " " + lastName;
+
+// let personInfoTwo = `I am ${fullName}. I am ${age}. I live in ${country}.`; //ES6 - Метод строковой интерполяции
+// let personInfoThree = `I am ${fullName}. I live in ${city}, ${country}. I am a ${job}. I teach ${language}.`;
+// console.log(personInfoTwo);
+// console.log(personInfoThree);
+
+// let a = 2;
+// let b = 3;
+// console.log('${a} is greater than ${b}:${a > b}');
+
+// let js = "JavaScript";
+// console.log(js.length); //10
+
+// let string = "JavaScript";
+// let fristLetter = string[1];
+// console.log(fristLetter);// J
+
+// let string = "JavaScript";
+// console.log(string.toUpperCase())
+
+// let string = "JavaScript";
+// console.log(string.toLowerCase()) // javascript
+
+
+// let string = "JavaScript";
+// console.log(string.substr(4, 6)); // Script
+
+
+// let string = "JavaScript";
+// console.log(string.substring(0,4));//Java
+
+// let string = "30 Days of JavaScript";
+// console.log(string.split());
+
+// let string = " 30 Days of JavaScript ";
+// console.log(string);
+// console.log(string.trim());
+
+// let string = "30 Days of JavaScript";
+// console.log(string.includes("Days")); //true
+// console.log(string.includes("days")); // false
+
+// let string = "30 Days of JavaScript";
+// console.log(string.replace("JavaScript", "Python"));
+
+
+// let string = "30 Days of JavaScript";
+// console.log(string.charAt(0)); //3
+
+// let string  = "30 Days of JavaScript";
+// console.log(string.charCodeAt(3)); // D ASCII number is 51
+
+
+// let string = "30 Days of JavaScript";
+// console.log(string.indexOf("D"));//3
+
+// let string = "I love JavaScript. If you do not love JavaScript what else can you love.";
+// console.log(string.lastIndexOf("love"));//67
+
+// let string = "30";
+// console.log(string.concat("Days","of","JavaScript")); //30DaysOfJavaScript
+
+// let string = "Love is the best to in this world";
+// console.log(string.startsWith("Love")); //true
+// console.log(string.startsWith("love")); //false
+
+// let string = "Love is the best to in this world";
+// console.log(string.endsWith("world")); //true
+// console.log(string.endsWith("lovae")); // false
+
+// let string = "I love JavaScript. If you do not love JavaScript what else can you love.";
+// console.log(string.search("love")); //2
+
+
+// let string = "I love JavaScript. If you do not love JavaScript what else can you love.";
+// console.log(string.match("love")); // 2
+
+// let string = "love";
+// console.log(string.repeat(10)); //// lovelovelovelovelovelovelovelovelovelove
+
+
+// let number = 5;
+// number > 0
+//   ? console.log(`${number} is a positive number`)
+//   : console.log(`${number} is a negative number`);
+// number = -5;
+
+// number > 0
+//   ? console.log(`${number} is a positive number`)
+//   : console.log(`${number} is a negative number`);
+// const now = new Date();
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getSeconds());
+
+// const agree = confirm("Are you sure you like to delete? ");
+// console.log(agree); // результат будет true или false в зависимости от того, что вы нажимаете в диалоговом окне
+// alert("Hello ");
+// let number = prompt("Enter number", "number goes here");
+// console.log(number);
+
